cd1986566c2aeeb353468cae5ffd039046828078
Muia23 <wilfred.muema@gmail.com>
2020-07-03 19:21:44 +0300

    define the Headline class

--- app/models/news.py
+++ app/models/news.py
@@ -1,9 +1,10 @@
-class News:
+class Headline:
     '''
     News class to define News Objects
     '''
 
-    def __init__(self,title,description,urlToImage,publishedAt,author):
+    def __init__(self,id,title,description,urlToImage,publishedAt,author):
+        self.id =id
         self.title = title
         self.description = description
         self.urlToImage = urlToImage
