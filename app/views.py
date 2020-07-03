11eedd96bd1fb73fa0523389456e6eda373d2058
Muia23 <wilfred.muema@gmail.com>
2020-07-03 19:16:52 +0300

    making the API call

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from .request import get_headlines
+
 
 # views
 @app.route('/')
@@ -7,4 +9,16 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    #Getting headline news
+    cnn_headlines = get_headlines('cnn')
+    print(cnn_headlines)
+    title = 'Home of Truthful and Unbiased News'
+    return render_template('index.html', title = title, cnn = cnn_headlines)
+
+@app.route('/news/<news_id>')
+def news(news_id):
+    '''
+    view news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
+    
