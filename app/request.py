097368cdec18f36ad0ce13536997717b8ecff1f8
Muia23 <wilfred.muema@gmail.com>
2020-07-03 16:07:44 +0300

    create a function that sends a request and process the response

--- app/request.py
+++ app/request.py
@@ -7,3 +7,21 @@ api_key = app.config['NEWS_API_KEY']
 
 # Getting the news base url
 base_url = app.config['HEADLINES_API_BASE_URL']
+
+def get_headlines(sources):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_headlines_url = base_url.format(sources,api_key)
+
+    with urllib.request.urlopen(get_headlines_url) as url:
+            get_headlines_data = url.read()
+            get_headlines_respone = json.loads(get_movies_data)
+
+            headlines_results = None
+
+            if get_headlines_respone['results']:
+                headline_results_list = get_headlines_respone['results']
+                headline_results = process_results(headline_results_list)
+
+    return headline_results
