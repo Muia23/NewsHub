459d0e7e85905a94bbb7eea35351ddf03937ae37
Muia23 <wilfred.muema@gmail.com>
2020-07-06 12:46:04 +0300

    add get line function to root page function

--- app/views.py
+++ app/views.py
@@ -10,10 +10,10 @@ def index():
     View root page function that returns the index page and its data
     '''
     #Getting headline news
-    cnn_headlines = get_headlines('cnn')
+    source_headlines = get_headlines('cnn')
     msnbc_headlines = get_headlines('msnbc')
     title = 'Home of Truthful and Unbiased News'
-    return render_template('index.html', title = title, cnn = cnn_headlines, msnbc = msnbc_headlines)
+    return render_template('index.html', title = title, source = source_headlines, msnbc = msnbc_headlines)
 
 @app.route('/news/<news_id>')
 def news(news_id):
