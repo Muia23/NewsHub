660281d4ec77b30b26020a59ff8d182c36d9183b
Muia23 <wilfred.muema@gmail.com>
2020-07-06 12:37:32 +0300

    initialized the news class

--- app/models/news.py
+++ app/models/news.py
@@ -4,7 +4,7 @@ class Headline:
     '''
 
     def __init__(self,id,title,description,urlToImage,publishedAt,author):
-        self.id =id
+        self.id =id    
         self.title = title
         self.description = description
         self.urlToImage = urlToImage
