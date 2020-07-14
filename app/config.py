75fcfee467c5f9d293c4656b69fbe288087a27a9
Muia23 <wilfred.muema@gmail.com>
2020-07-14 10:46:56 +0300

    add a source based api url

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ class Config:
     General configuration parent class
     '''
     HEADLINES_API_BASE_URL ='https://newsapi.org/v2/top-headlines?country={}&apiKey={}'
+    SOURCES_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&country={}&apiKey={}'
 
 class ProdConfig(Config):
