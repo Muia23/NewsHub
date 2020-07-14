c60e17bf554a5815a1b296700fdb382eb9bd0982
Muia23 <wilfred.muema@gmail.com>
2020-07-14 10:49:53 +0300

    added functions to to get and process the response from the source url request

--- app/request.py
+++ app/request.py
@@ -1,6 +1,7 @@
 from app import app
 import urllib.request,json
 from .models import news
+import datetime
 
 Headline = news.Headline
 News = news.News
@@ -9,13 +10,14 @@ News = news.News
 api_key = app.config['NEWS_API_KEY']
 
 # Getting the news base url
-base_url = app.config["HEADLINES_API_BASE_URL"]
+headline_url = app.config["HEADLINES_API_BASE_URL"]
+source_url = app.config["SOURCES_API_BASE_URL"]
 category_url =app.config["NEWS_API_BASE_URL"]
 def get_headlines(country):
     '''
     Function that gets the json response to our url request
     '''
-    get_headlines_url = base_url.format(country,api_key)
+    get_headlines_url = headline_url.format(country,api_key)
 
     with urllib.request.urlopen(get_headlines_url) as url:
         get_headlines_data = url.read()
@@ -34,7 +36,7 @@ def process_results(headline_list):
     Function that process the headline result and transform them to a list of Objects
 
     Args:
-        headline_list: A list of dictionaries that containt headlines details
+        headline_list: A list of dictionaries that contain headlines details
 
     Returns:
         headline_results: A list of headline objects
@@ -55,27 +57,47 @@ def process_results(headline_list):
     
     return headline_results
 
-def get_headline(country):
-    get_healine_details_url = base_url.format(country,api_key)
+def get_station(source):
+    get_station_url = source_url.format(source,api_key)
 
-    with urllib.request.urlopen(get_headline_details_url) as url:
-        headline_details_data = url.read()
-        headline_details_response = json.loads(headline_details_data)
+    with urllib.request.urlopen(get_station_url) as url:
+        get_station_data = url.read()
+        get_station_response = json.loads(get_station_data)
 
-        headline_object = None
-        if headline_details_response:
-            id = headline_details_response.get('id')        
-            title = headline_details_response.get('title')
-            description = headline_details_response.get('description')
-            urlToImage = headline_details_response.get('urlToImage')
-            publishedAt = headline_details_response.get('publishedAt')
-            author = headline_details_response.get('author')
-            content = headline_details_response.get('content')
-            content = headline_details_response.get('url') 
+        station_results = None
 
-            headline_object = Headline(id,title,description,urlToImage,publishedAt,author,content,url)
+        if get_station_response['articles']:
+            station_results_list = get_station_response['articles']
+            station_results = process_station(station_results_list)
+            
+    return station_results
 
-    return headline_object
+def process_station(station_list):
+    '''
+    Function that process the headline result and transform them to a list of Ojects
+
+    Args:
+        station_list: A list of dictionaries that contain the stations' news
+
+    Returns:
+        station_results: A list of stations news objects
+    '''
+    station_results = []
+    for station_item in station_list:
+        id = station_item.get('id')        
+        title = station_item.get('title')
+        description = station_item.get('description')
+        urlToImage = station_item.get('urlToImage')
+        publishedAt = station_item.get('publishedAt')
+        author = station_item.get('author')
+        content = station_item.get('content')
+        url = station_item.get('url') 
+
+        if urlToImage:
+            station_object = Headline(id,title,description,urlToImage,publishedAt,author,content,url)
+            station_results.append(station_object)
+
+    return station_results
 
 #category news
 def get_category_news(category,country):
@@ -118,20 +140,20 @@ def process_category_results(category_new_list):
     
     return category_new_results
 
-def get_category_new(category,country):
-    get_category_new_details_url = category_url.format(category,country,api_key)
-
-    with urllib.request.urlopen(get_category_new_details_url) as url:
-        category_new_details_data = url.read()
-        category_new_details_response = json.loads(category_new_details_data)
-
-        category_new_object = None
-        if category_new_details_response:
-            id = category_new_details_response.get('id')        
-            name = category_new_details_response.get('name')
-            description = category_new_details_response.get('description')            
-            url = category_new_details_response.get('url') 
-
-            category_new_object = News(id,name,description,url)
-
-    return category_new_object
+#def get_category_new(category,country):
+#    get_category_new_details_url = category_url.format(category,country,api_key)
+#
+#    with urllib.request.urlopen(get_category_new_details_url) as url:
+#        category_new_details_data = url.read()
+#        category_new_details_response = json.loads(category_new_details_data)
+#
+#        category_new_object = None
+#        if category_new_details_response:
+#            id = category_new_details_response.get('id')        
+#            name = category_new_details_response.get('name')
+#            description = category_new_details_response.get('description')            
+#            url = category_new_details_response.get('url') 
+#
+#            category_new_object = News(id,name,description,url)
+#
+#    return category_new_object
